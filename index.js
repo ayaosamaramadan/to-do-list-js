247b1f41459ca84fac9f5ffe6334eecdcc587fb6
Aya Osama <ayaramadan2011@gmail.com>
2024-12-11 19:08:11 +0200

    style tasks

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let openTask = document.getElementById("task-btn");
 let addTask = document.getElementById("add-btn");
+let submitTask = document.getElementById("submit");
+let taskInput = document.getElementById("inputt");
+
+let tasks = [];
 
 addTask.style.opacity = "0";
+openTask.style.cssText = "visibility:visible";
 
 openTask.addEventListener("click", () => {
     addTask.style.opacity = "1";
-    openTask.style.opacity = "0";
+    openTask.style.cssText = "visibility:hidden";
 });
+
+submitTask.addEventListener("click", () => {
+  
+});
+
 
