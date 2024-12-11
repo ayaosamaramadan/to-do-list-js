9efaf48f4973cb9860448875f26b0252938e090c
Aya Osama <ayaramadan2011@gmail.com>
2024-12-11 17:50:38 +0200

    style elements

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ let addTask = document.getElementById("add-btn");
 addTask.style.opacity = "0";
 
 openTask.addEventListener("click", () => {
-  if (addTask.style.opacity === "0") {
     addTask.style.opacity = "1";
-  } else {
-    addTask.style.opacity = "0";
-  }
+    openTask.style.opacity = "0";
 });
 
