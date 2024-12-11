2b4a7e174a3e534247df50dce889bc9cd054f0dc
Aya Osama <ayaramadan2011@gmail.com>
2024-12-11 17:32:00 +0200

    handle openTask

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let aa= document.getElementById('a');
+let openTask = document.getElementById("task-btn");
+let addTask = document.getElementById("add-btn");
 
-aa.addEventListener('click', function(){
-  console.log('click');
+addTask.style.opacity = "0";
+
+openTask.addEventListener("click", () => {
+  if (addTask.style.opacity === "0") {
+    addTask.style.opacity = "1";
+  } else {
+    addTask.style.opacity = "0";
+  }
 });
+
